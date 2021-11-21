31a6d24f68bac9286e504fb50c9b7df7fbc5bd77
AkbarAli <88106359+DataAkbar@users.noreply.github.com>
2021-11-21 16:58:22 +0500

    Kitop boshlandi

---
+++ NicholaLacey/Part1-PythonniOrganamiz/Osnovi/Mashqlar/mashqlar001_011.py
@@ -0,0 +1,79 @@
+"""Misollar"""
+
+# 001 
+
+name = input("O'z ismingizni kiriting: ")
+print("Salom", name)
+
+# 002
+
+name = input("O'z ismingizni kiriting: ")
+lastname = input("O'z familiyangizni kiriting: ")
+print("Salom!,", name, lastname)
+
+# 003
+
+print("What do you call a bear with no teeth\nA gummy bear!")
+
+# 004 
+
+rec0 = input("So'n kiriting: ")
+rec1 = input("Yana bir so'n kiriting: ")
+
+print("The total is", rec0 + rec1)
+
+# 005 
+
+rec0 = int(input("So'n kiriting: "))
+rec1 = int(input("Yana bir so'n kiriting: "))
+rec2 = int(input("Yana bir so'n kiriting: "))
+
+print("The answer is", (rec0 + rec1)*rec2)
+
+# 006
+
+message0 = int(input("Sizda necha Pitsiya qoldig'i bor edi?: "))
+message1 = int(input("Siz nechta Pitsiya qoldig'ini yedinigiz?: "))
+runi = message0 - message1
+print(f"Sizda {runi}ta Pitsiya qoldig'i qoldi")
+
+# 007 
+
+name = input("Sizning isminig nima? ")
+age = int(input("Siz necha youshdasiz? "))
+newAge = age + 1
+print(name,"," 'Sizning keyingi yoshingiz', newAge)
+
+# 008
+
+bill = int(input("What is the total cost of the bill? "))
+
+people = int(input("how many people are there? "))
+
+each = bill / people
+
+print("Each person should pay $", each)
+
+# 009
+
+day = int(input("Enter the number of days: "))
+hours = day *24
+minutes = hours * 60
+seconds = minutes * 60
+print("In", day, " Days there are... ")
+print(hours, "hours")
+print(minutes, "minutes")
+print(seconds, "seconds")
+
+# 010
+
+kilo = int(input("Enter the number of kilos: "))
+pound = kilo * 2.204
+print("That is ", pound, " pounds")
+
+# 011
+
+large  = int(input("Enter a number 100: "))
+smaller = int(input("Enter a number 10: "))
+answeri = large // smaller 
+print(smaller, " goes into ", large, answer, " times")
